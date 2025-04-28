1cda0e6c565e9ae1061f8a73b2dbff1759d12162
ubeydullah doğan <ubey_52@hotmail.com>
2025-04-28 08:55:46 +0300

    anasayfa düzenleme

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -9,35 +9,20 @@
 				<div class="row">
 					<div class="col-md-6 col-sm-12">
 						<div class="title">
-							<h4>DataTable</h4>
+							<h4>Gerçek Kişi Listesi</h4>
 						</div>
 						<nav aria-label="breadcrumb" role="navigation">
 							<ol class="breadcrumb">
 								<li class="breadcrumb-item">
-									<a href="index.html">Home</a>
+									<a href="index.html">Anasayfa</a>
 								</li>
 								<li class="breadcrumb-item active" aria-current="page">
-									DataTable
+									Liste
 								</li>
 							</ol>
 						</nav>
 					</div>
-					<div class="col-md-6 col-sm-12 text-right">
-						<div class="dropdown">
-							<a
-								class="btn btn-primary dropdown-toggle"
-								href="#"
-								role="button"
-								data-toggle="dropdown">
-								January 2018
-							</a>
-							<div class="dropdown-menu dropdown-menu-right">
-								<a class="dropdown-item" href="#">Export List</a>
-								<a class="dropdown-item" href="#">Policies</a>
-								<a class="dropdown-item" href="#">View Assets</a>
-							</div>
-						</div>
-					</div>
+				
 				</div>
 			</div>
 			<!-- Simple Datatable start -->
@@ -56,7 +41,7 @@
 
 			<div class="card-box mb-30">
 				<div class="pd-20">
-					<h4 class="text-blue h4">Data Table with Export Buttons</h4>
+					<h4 class="text-blue h4">Gerçek Kişi Liste</h4>
 				</div>
 				<div class="pb-20">
 					<table
