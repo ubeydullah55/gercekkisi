02d2ce20123190a42582d4915b22047e5b60313b
ubeydullah doğan <ubey_52@hotmail.com>
2025-04-29 14:41:07 +0300

    logo

--- app/Views/include/header.php
+++ app/Views/include/header.php
@@ -97,7 +97,7 @@
 		<div class="pre-loader">
 			<div class="pre-loader-box">
 				<div class="loader-logo">
-					<img src="<?= base_url("assets/") ?>vendors/images/sarlogo.png" alt="" />
+					<img src="<?= base_url("assets/") ?>vendors/images/deskapp-logo.svg" alt="" />
 				</div>
 				<div class="loader-progress" id="progress_div">
 					<div class="bar" id="bar1"></div>
